0ed34b2f628c0c9f2b05a22bb21d6e5c8017c652
kamuridesu <myk.gata14@gmail.com>
2021-10-19 14:52:18 -0300

    creating 2 vm instances with count

--- azure/resources.tf
+++ azure/resources.tf
@@ -1,51 +1,54 @@
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "resource_group" {
   name     = "example-resources"
   location = "West Europe"
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-
-    public_ip_address_id = azurerm_public_ip.public_ip.id
-  }
-}
-
 resource "azurerm_public_ip" "public_ip" {
-  name                = "accesptanceTestPublicIp1"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  count = var.number_of_instances
+  name                = "${count.index}-public_ip_instance"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location            = azurerm_resource_group.resource_group.location
   allocation_method   = "Static"
 }
 
+resource "azurerm_virtual_network" "virtual_network" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.resource_group.name
+  virtual_network_name = azurerm_virtual_network.virtual_network.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "network_interface" {
+  count = var.number_of_instances
+  name                = "${count.index}-example-nic"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+
+    public_ip_address_id = azurerm_public_ip.public_ip[count.index].id
+  }
+}
+
 resource "azurerm_linux_virtual_machine" "example" {
-  name                = "example-machine"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  count               = var.number_of_instances
+  name                = "${count.index}-example-machine"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location            = azurerm_resource_group.resource_group.location
   size                = "Standard_F2"
   admin_username      = var.username
   network_interface_ids = [
-    azurerm_network_interface.example.id,
+    azurerm_network_interface.network_interface[count.index].id,
   ]
 
   admin_ssh_key {
@@ -81,7 +84,7 @@ resource "azurerm_linux_virtual_machine" "example" {
       type        = "ssh"
       user        = "adminuser"
       private_key = file("/home/kamuri/.ssh/id_rsa")
-      host        = azurerm_public_ip.public_ip.ip_address
+      host        = azurerm_public_ip.public_ip[count.index].ip_address
     }
   }
 }
--- azure/variables.tf
+++ azure/variables.tf
@@ -7,3 +7,9 @@ variable "username" {
   description = "vm username"
   type        = string
 }
+
+variable "number_of_instances" {
+  description = "Number os instances to be created"
+  type        = number
+  default     = 2
+}
