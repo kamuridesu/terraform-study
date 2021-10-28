de6e0e864ba0c33c34de52e30c7b63d3a42a6aa1
kamuridesu <myk.gata14@gmail.com>
2021-10-28 15:58:49 -0300

    started aws tf

---
+++ aws/aws.tf
@@ -0,0 +1,22 @@
+terraform {
+    required_providers {
+      aws = {
+          source = "hashicorp/aws"
+          version = "~> 3.0"
+      }
+    }
+}
+
+provider "aws" {
+    profile = "default"
+    region = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+    ami = "ami-07d02ee1eeb0c996c"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "ExampleAppServerInstance"
+    }
+}
